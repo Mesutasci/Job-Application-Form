ce8c91a50321e16c15d20a7fa5a02dd24c4b4966
Mesut Asci <67346634+Mesutasci@users.noreply.github.com>
2020-12-07 17:48:46 +0100

    Add files via upload

--- formvali.js
+++ formvali.js
@@ -1,14 +1,28 @@
-function usernameCheck(){
-    var regUsername = /^[a-zA-Z0-9]{2,15}$/;
-    var usernameValue = document.getElementById("username").value;
+function fnameCheck(){
+    var regUsername = /^[a-zA-Z0-9\s]{2,15}$/;
+    var fnameValue = document.getElementById("fname").value;
 
-    if (regUsername.test(usernameValue)){
-        document.getElementById('usernameNote').innerHTML = " Valid input";
-        document.getElementById('usernameNote').style.color = "green";
+    if (regUsername.test(fnameValue)){
+        document.getElementById('fnameNote').innerHTML = " Valid input";
+        document.getElementById('fnameNote').style.color = "green";
     }
     else{
-        document.getElementById("usernameNote").innerHTML = " Invalid number or type of characters! Please try again.";
-        document.getElementById('usernameNote').style.color = "red";
+        document.getElementById("fnameNote").innerHTML = " Invalid number or type of characters! Please try again.";
+        document.getElementById('fnameNote').style.color = "red";
+    }
+}
+
+function lnameCheck(){
+    var regUsername = /^[a-zA-Z0-9\s]{2,15}$/;
+    var lnameValue = document.getElementById("lname").value;
+
+    if (regUsername.test(lnameValue)){
+        document.getElementById('lnameNote').innerHTML = " Valid input";
+        document.getElementById('lnameNote').style.color = "green";
+    }
+    else{
+        document.getElementById("lnameNote").innerHTML = " Invalid number or type of characters! Please try again.";
+        document.getElementById('lnameNote').style.color = "red";
     }
 }
 
@@ -38,19 +52,6 @@ function emailConfirm(){
     }
 }
 
-function fnameCheck(){
-    var regName = /[a-zA-Z_\s]{4,}$/;
-    var fnameValue = document.getElementById("fname").value;
-
-    if (regName.test(fnameValue)){
-        document.getElementById('fnameNote').innerHTML = " Valid input";
-        document.getElementById('fnameNote').style.color = " green";
-    }
-    else{
-        alert("Something is wrong! Please enter you firstname and lastname.");
-    }
-}
-
 function genderSelect(){
     var option = document.getElementsByName('gender');
     if (!(option[0].checked || option[1].checked || option[2].checked)){
@@ -59,21 +60,6 @@ function genderSelect(){
     else{
         return false;
     }
-}
-
-function checkArea(){
-    if(document.getElementById('experience').value === ''){      
-        alert("Please Provide Details!");
-              document.getElementById('experience').focus();
-        return false;       
-    }
-    else if(document.getElementById('name').value == ''){      
-        alert("Please Provide Name!");
-            document.getElementById('name').focus();
-        return false;       
-    }
-    else
-        return true;
 }
 
 function check_phone(){
@@ -108,6 +94,25 @@ function addressCheck(){
     else
         return true;
 }
+
+
+// Validation for textareas
+function checkArea(){
+    if(document.getElementById('explanation').value === ''){      
+        alert("Please Provide Details!");
+              document.getElementById('explanation').focus();
+        return false;       
+    }
+    else if(document.getElementById('name').value == ''){      
+        alert("Please Provide Name!");
+            document.getElementById('name').focus();
+        return false;       
+    }
+    else
+        return true;
+}
+
+
         
 
 
